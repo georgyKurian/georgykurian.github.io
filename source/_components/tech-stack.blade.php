7d52630f567fba94dd3a152e2e2fa47103c1d674
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-08-20 22:55:44 -0400

    Skills section design change

--- source/_components/tech-stack.blade.php
+++ source/_components/tech-stack.blade.php
@@ -1,24 +1,18 @@
-<div class="w-full lg:w-1/3 flex">
-    <div class="w-full rounded-lg m-4 bg-gray-800 h-100 overflow-hidden shadow-lg">
-        <div class="flex bg-gray-800 px-3 py-2 text-center overflow-hidden shadow-lg text-white">
-            {{ $svg_icon }}
+<div class="w-full lg:w-auto flex">
+    <div class="w-full rounded-lg m-4 bg-gray-800 border-gray-300 border h-100 overflow-hidden shadow-lg">
+        <div class="flex bg-gray-800 px-5 py-3 text-center overflow-hidden shadow-lg text-white">
+            <span class="my-auto font-medium">{{ $title }}</span>
         </div>
-        <div
-            class="bg-gray-800 text-white p-5 rounded-b flex flex-col justify-between leading-normal">
-
-            <table class="w-full">
+        <div class="bg-white px-5 py-3 text-gray-800 h-full rounded-t-lg flex flex-col justify-between leading-normal">
+            <div class="w-full">
                 @foreach ($skills as $skill)
-                <tr>
-                    <td class="w-1/2 py-1 font-semibold text-sm">{{ $skill['name'] }}</td>
-                    <td>
-                        <div class="w-full h-full rounded-sm bg-gray-700 overflow-hidden">
-                            <div class="float-left rounded-sm text-white h-full bg-blue-500 pr-1 font-semibold text-xs text-right"
-                        style="width:{{ $skill['percentage'] }};">{{ $skill['percentage'] }}</div>
-                        </div>
-                    </td>
-                </tr>
+                <div class="py-1 text-sm font-semibold">{{ $skill['name'] }}
+                    @if ($skill['frameworks'])
+                    - {{ implode($skill['frameworks'],', ') }}
+                    @endif
+                </div>
                 @endforeach
-            </table>
+            </div>
         </div>
     </div>
 </div>
