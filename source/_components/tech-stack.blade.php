410ca9b9f6f85e0a002da77baca12e8526d97706
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-08-20 20:32:39 -0400

    Added code skills

--- source/_components/tech-stack.blade.php
+++ source/_components/tech-stack.blade.php
@@ -1,19 +1,19 @@
 <div class="w-full lg:w-1/3 flex">
-    <div class="w-full rounded-lg overflow-hidden shadow-lg">
+    <div class="w-full rounded-lg m-4 bg-gray-800 h-100 overflow-hidden shadow-lg">
         <div class="flex bg-gray-800 px-3 py-2 text-center overflow-hidden shadow-lg text-white">
             {{ $svg_icon }}
         </div>
         <div
-            class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            class="bg-gray-800 text-white p-5 rounded-b flex flex-col justify-between leading-normal">
 
             <table class="w-full">
                 @foreach ($skills as $skill)
                 <tr>
-                    <td>{{ $skill->name }}</td>
+                    <td class="w-1/2 py-1 font-semibold text-sm">{{ $skill['name'] }}</td>
                     <td>
-                        <div class="w-full h-5  bg-gray-300">
-                            <div class="float-left h-full bg-blue-400 pr-1 font-semibold text-xs text-right"
-                                style="width:80%;">80%</div>
+                        <div class="w-full h-full rounded-sm bg-gray-700 overflow-hidden">
+                            <div class="float-left rounded-sm text-white h-full bg-blue-500 pr-1 font-semibold text-xs text-right"
+                        style="width:{{ $skill['percentage'] }};">{{ $skill['percentage'] }}</div>
                         </div>
                     </td>
                 </tr>
