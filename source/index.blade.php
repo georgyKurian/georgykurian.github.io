2a44846b91d8a15673c0a77343883fea3020457b
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-08-27 22:41:50 -0400

    Minor Style changes

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,8 +4,13 @@
 <div class="relative w-full h-full">
     <div class="flex justify-around container mx-auto my-48">
         <div class="flex-1">
-            <h1 class="text-center text-gray-100 font-normal">Full Stack Developer</h1>
-            <h2 class="text-center text-gray-200 font-normal mt-1">Open source technologies enthusiast</h2>
+            <h1 class="text-center text-gray-100 font-normal">Hi, I’m Georgi. Nice to meet you!</h1>
+            <p class="text-center text-gray-200">Since beginning my journey as a freelance designer nearly 8 years ago,
+                I've done remote
+                work for agencies, consulted for startups, and collaborated with talented people to create digital
+                products
+                for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on
+                improving my chops one design problem at a time.</p>
         </div>
     </div>
     {{--  <div class="absolute bottom-0 inset-x-0 bg-cover bg-center h-56" style="background-image: url('/assets/img/hero-divider.svg')">&nbsp;</div> --}}
@@ -13,29 +18,22 @@
 @endsection
 
 @section('body')
-<div class="bg-gray-200 whiteGradient">
-    <div class="container flex-1 max-w-4xl mx-auto py-20 px-6 md:py-16">
-        <h3 class="text-center">Hi, I’m Georgi. Nice to meet you.</h3>
-        <p class="text-center">Since beginning my journey as a freelance designer nearly 8 years ago, I've done remote
-            work for agencies, consulted for startups, and collaborated with talented people to create digital products
-            for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.</p>
-    </div>
-</div>
 
 <!-- Timeline -->
-<div class="flex whiteGradient">
-    <div class="container flex flex-wrap justify-center max-w-5xl mx-auto py-20 px-6 md:py-16 text-gray-800 ">
+<div class="flex whiteGradient border-b-2 border-gray-400 py-16">
+    <div class="container flex flex-wrap justify-center max-w-5xl mx-auto px-6 md:py-16 text-gray-800 ">
+        <h2 class="text-center mb-12">Experience</h2>
         @component('_components.timeline',[
-            'experience' => $page->getjsondata('my_work_experience.json')
+        'experience' => $page->getjsondata('my_work_experience.json')
         ])
         @endcomponent
     </div>
 </div>
 
 <!-- Projects -->
-<div class="flex whiteGradient">
-    <div class="container flex flex-wrap max-w-5xl mx-auto py-20 px-6 md:py-16 text-gray-800">
+<div class="flex whiteGradient border-b-2 border-gray-400 py-16">
+    <div class="container flex flex-wrap justify-center max-w-5xl mx-auto px-6 md:py-16 text-gray-800 ">
+        <h2 class="text-center mb-12">Projects</h2>
         @foreach ($page->getjsondata('my_projects.json') as $project)
         @component('_components.project',[
         'project' => $project
@@ -46,8 +44,9 @@
 </div>
 
 <!-- Skill -->
-<div class="flex whiteGradient">
-    <div class="container flex flex-wrap justify-center max-w-5xl mx-auto py-20 px-6 md:py-16 text-gray-800 ">
+<div class="flex whiteGradient border-b-2 border-gray-400 py-16">
+    <div class="container flex flex-wrap justify-center max-w-5xl mx-auto px-6 md:py-16 text-gray-800 ">
+        <h2 class="text-center mb-12 w-full">Skills</h2>
         @foreach ($page->getjsondata('my_skills.json') as $key => $item)
         @component('_components.tech-stack',[
         'title' => $key,
