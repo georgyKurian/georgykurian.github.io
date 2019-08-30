462f6161c1475f57eb69c3600097e7812ced05d6
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2019-08-29 22:44:48 -0400

    Added social media icons

--- source/_components/timeline.blade.php
+++ source/_components/timeline.blade.php
@@ -6,7 +6,7 @@
         <div class="rounded-full h-4 w-4 border-4 border-blue-500 {{ $loop->first ? 'bg-blue-500':'' }}"></div>
         <div class="flex-1 w-1 {{ !$loop->last ? 'bg-blue-500':'' }}">&nbsp;</div>
     </div>
-    <div class="flex pl-3 pb-10 w-1/2">
+    <div class="flex pl-3 pb-10 w-2/3">
         <div>
             <div class="text-gray-700">
                 <a href="https://www.lambtoncollege.ca/Applied-Research/" target="_blank">
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,17 @@
 @extends('_layouts.master')
 
 @section('extra-header')
-<div class="relative w-full h-full">
-    <div class="flex justify-around container mx-auto my-48">
-        <div class="flex-1">
+<div class="flex items-center w-full">
+    <div class="flex flex-col items-center justify-around container mx-auto mt-40 mb-16">
+        <div>
             <h1 class="text-center text-gray-100 font-normal">Hi, I’m Georgi. Nice to meet you!</h1>
             <p class="text-center text-gray-200">I am developer who is ethusiatic about finding solution and performance optimization.</p>
+        </div>
+        <div class="flex flex-wrap w-full justify-center md:flex-row py-16">
+            <a href="https://github.com/georgyKurian" title="GitHub profile link" class="w-1/4 md:w-12 m-2"><img src="assets/img/logos/github.svg" alt="GitHub" class="w-full"></a>
+            <a href="https://linkedin.com/in/georgi-kurian/" title="Linkedin profile link" class="w-1/4 md:w-12 m-2"><img src="assets/img/logos/linkedin.svg" alt="Linkedin" class="w-full"></a>
+            <a href="mailto:georgyvk@gmail.com" title="Email to georgyvk@gmail.com" class="w-1/4 md:w-12 m-2"><img src="assets/img/logos/email.svg" alt="Email" class="w-full"></a>
+            <a href="https://stackoverflow.com/users/10349395/georgy-varghese-kurian" title="Stackoverflow profile link" class="w-1/4 md:w-12 m-2"><img src="assets/img/logos/stackoverflow.svg" alt="Stackoverflow" class="w-full"></a>
         </div>
     </div>
     {{--  <div class="absolute bottom-0 inset-x-0 bg-cover bg-center h-56" style="background-image: url('/assets/img/hero-divider.svg')">&nbsp;</div> --}}
